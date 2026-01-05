cdcf575bbada7f6ff2a49147e2047f01ff97a3ea
nehaldamor <168215150+nehaldamor@users.noreply.github.com>
2026-01-05 11:51:09 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
-const connectDB = require("./db/db");
 
-connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
